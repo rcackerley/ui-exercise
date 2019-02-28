33b9ee01679f74961fcd01f83dff54ef0af34194
Robby Ackerley <rcackerley@me.com>
2019-02-27 20:27:48 -0500

    create project structure, add dependencies

---
+++ src/design/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    white: "#ffffff",
+    grey: "#666",
+    black: "#1f1f1f",
+    primary: "#d93025",
+    secondary: "#1a73e8",
+    tertiary: "#1e8e3e"
+  },
+  text: {
+    family: "Roboto,RobotoDraft,Helvetica,Arial,sans-serif"
+  }
+}
