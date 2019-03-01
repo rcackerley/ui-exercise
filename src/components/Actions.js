454fb72b497288c1f3be2e797ec507a46fe7f789
Robby Ackerley <rcackerley@me.com>
2019-02-28 22:25:22 -0500

    add an action for delete multiple

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -2,8 +2,36 @@
 
 import React from "react"
 
-import Column from "../design/Column"
+import Row from "../design/Row"
+import IconButton from "../design/IconButton"
+import { theme } from "../design/theme"
 
-const Actions = () => <div />
+import ReactSVG from "react-svg"
 
-export default Actions
+import injectSheet from "react-jss"
+
+import trash from "../assets/icons/baseline-delete.svg"
+
+const styles = {
+  row: {
+    borderBottom: `1px solid ${theme.colors.background}`,
+    paddingLeft: "15px"
+  },
+  icon: {
+    fill: theme.colors.grey
+  }
+}
+
+type Props = {|
+  classes: { [string]: string }
+|}
+
+const Actions = ({ classes }: Props) => (
+  <Row customStyles={classes.row}>
+    <IconButton>
+      <ReactSVG svgClassName={classes.icon} src={trash} />
+    </IconButton>
+  </Row>
+)
+
+export default injectSheet(styles)(Actions)
